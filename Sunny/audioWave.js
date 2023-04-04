c5959fd6173f6ea1ad714ddf359ba9b5c547e243
hcho109 <hcho109@aucklanduni.ac.nz>
2023-04-05 00:35:37 +1200

    Add a document for Annotation webpage design update
    Record changes made on the annotation webpage

--- Sunny/audioWave.js
+++
@@ -1,8 +0,0 @@
-var audioWaveTrack = WaveSurfer.create({
-    container: '.audio',
-    waveColor: 'lightgray',
-    progressColor: 'red',
-    barWidth: 2
-});
-
-audioWaveTrack.load('C:\Users\gosun\Desktop\2023 S1\COMPSYS 700A_B\webApp\P4P_-26\Sunny\test_annotation.mp3');
