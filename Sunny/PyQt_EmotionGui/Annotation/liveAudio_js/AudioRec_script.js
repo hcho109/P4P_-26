d2b78a4044517d22c9bea2eade17fabbaf6a6040
hcho109 <hcho109@aucklanduni.ac.nz>
2023-07-18 05:50:53 +1200

    make audio recording downloadable and display waveform

---
+++ Sunny/PyQt_EmotionGui/Annotation/liveAudio_js/AudioRec_script.js
@@ -0,0 +1,216 @@
+const startRecordingButton = document.getElementById("startRecording");
+const stopRecordingButton = document.getElementById("stopRecording");
+const saveRecordingButton = document.getElementById("saveRecording");
+const saveWaveformButton = document.getElementById("saveWaveform");
+const saveSpectrogramButton = document.getElementById("saveSpectrogram");
+const isRecordingStatus = document.getElementById("isRecording");
+const waveformCanvas = document.getElementById("canvas_waveform");
+const spectrogramCanvas = document.getElementById("canvas_spectrogram");
+const audioElement = document.getElementById("audioElement");
+
+let isRecording = false;
+let audioChunks = [];
+let mediaRecorder = null;
+let mediaStream = null;
+let audioContext = null;
+let sourceNode = null;
+let analyser = null;
+let bufferLength = 0;
+let waveformCanvasCtx = waveformCanvas.getContext("2d");
+let spectrogramCanvasCtx = spectrogramCanvas.getContext("2d");
+
+startRecordingButton.addEventListener("click", startRecording);
+stopRecordingButton.addEventListener("click", stopRecording);
+saveRecordingButton.addEventListener("click", saveRecording);
+saveWaveformButton.addEventListener("click", saveWaveform);
+saveSpectrogramButton.addEventListener("click", saveSpectrogram);
+
+function startRecording() {
+  isRecording = true;
+  audioChunks = [];
+
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then(function (stream) {
+      mediaStream = stream;
+      audioContext = new AudioContext();
+      sourceNode = audioContext.createMediaStreamSource(mediaStream);
+      analyser = audioContext.createAnalyser();
+      analyser.fftSize = 2048;
+      bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+
+      sourceNode.connect(analyser);
+      analyser.connect(audioContext.destination);
+
+      const frequencyDataArray = new Uint8Array(bufferLength);
+
+      function drawWaveform() {
+        if (!isRecording) {
+          return;
+        }
+
+        requestAnimationFrame(drawWaveform);
+
+        analyser.getByteTimeDomainData(dataArray);
+
+        waveformCanvasCtx.fillStyle = "rgb(255, 255, 255)";
+        waveformCanvasCtx.fillRect(
+          0,
+          0,
+          waveformCanvas.width,
+          waveformCanvas.height
+        );
+
+        waveformCanvasCtx.lineWidth = 2;
+        waveformCanvasCtx.strokeStyle = "rgb(0, 0, 0)";
+
+        waveformCanvasCtx.beginPath();
+
+        const sliceWidth = waveformCanvas.width * 1.0 / bufferLength;
+        let x = 0;
+
+        for (let i = 0; i < bufferLength; i++) {
+          const v = dataArray[i] / 128.0;
+          const y = (v * waveformCanvas.height) / 2;
+
+          if (i === 0) {
+            waveformCanvasCtx.moveTo(x, y);
+          } else {
+            waveformCanvasCtx.lineTo(x, y);
+          }
+
+          x += sliceWidth;
+        }
+
+        waveformCanvasCtx.lineTo(
+          waveformCanvas.width,
+          waveformCanvas.height / 2
+        );
+        waveformCanvasCtx.stroke();
+      }
+
+      drawWaveform();
+
+      function drawSpectrogram() {
+        if (!isRecording) {
+          return;
+        }
+
+        requestAnimationFrame(drawSpectrogram);
+
+        analyser.getByteFrequencyData(frequencyDataArray);
+
+        spectrogramCanvasCtx.fillStyle = "rgb(0, 0, 0)";
+        spectrogramCanvasCtx.fillRect(
+          0,
+          0,
+          spectrogramCanvas.width,
+          spectrogramCanvas.height
+        );
+
+        const barWidth = spectrogramCanvas.width / bufferLength;
+
+        for (let i = 0; i < bufferLength; i++) {
+          const value = frequencyDataArray[i];
+          const percent = value / 255;
+          const height = spectrogramCanvas.height * percent;
+          const offset = spectrogramCanvas.height - height - 1;
+
+          const hue = (i / bufferLength) * 360;
+          const color = `hsl(${hue}, 100%, 50%)`;
+
+          spectrogramCanvasCtx.fillStyle = color;
+          spectrogramCanvasCtx.fillRect(
+            i * barWidth,
+            offset,
+            barWidth,
+            height
+          );
+        }
+      }
+
+      drawSpectrogram();
+
+      mediaRecorder = new MediaRecorder(mediaStream);
+      mediaRecorder.start();
+
+      mediaRecorder.addEventListener("dataavailable", function (event) {
+        if (event.data.size > 0) {
+          audioChunks.push(event.data);
+        }
+      });
+
+      isRecordingStatus.textContent = "Recording...";
+    })
+    .catch(function (error) {
+      console.error("Error accessing microphone:", error);
+    });
+}
+
+function stopRecording() {
+  isRecording = false;
+
+  if (mediaRecorder) {
+    mediaRecorder.stop();
+    mediaRecorder = null;
+  }
+
+  if (mediaStream) {
+    mediaStream.getTracks().forEach(function (track) {
+      track.stop();
+    });
+    mediaStream = null;
+  }
+
+  isRecordingStatus.textContent = "Click 'Start' button to record";
+
+  if (audioChunks.length > 0) {
+    const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
+    const audioURL = URL.createObjectURL(audioBlob);
+    audioElement.src = audioURL;
+    audioElement.controls = true;
+    audioElement.load(); // Load the audio for playback
+  }
+}
+
+function saveRecording() {
+  if (audioChunks.length === 0) {
+    console.log("No recorded audio available.");
+    return;
+  }
+
+  const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(audioBlob);
+  link.download = "recorded_audio.wav";
+  link.click();
+}
+
+function saveWaveform() {
+  const waveformURL = waveformCanvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = waveformURL;
+  link.download = "waveform.png";
+  link.click();
+}
+
+function saveSpectrogram() {
+  const tempCanvas = document.createElement("canvas");
+  tempCanvas.width = spectrogramCanvas.width;
+  tempCanvas.height = spectrogramCanvas.height;
+  const tempCtx = tempCanvas.getContext("2d");
+  tempCtx.drawImage(
+    spectrogramCanvas,
+    0,
+    0,
+    tempCanvas.width,
+    tempCanvas.height
+  );
+
+  const spectrogramURL = tempCanvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = spectrogramURL;
+  link.download = "spectrogram.png";
+  link.click();
+}
--- Sunny/PyQt_EmotionGui/Annotation/liveAudio_js/rec_script.js
+++ Sunny/PyQt_EmotionGui/Annotation/liveAudio_js/rec_script.js
@@ -1,3 +1,92 @@
+"use strict";
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+class renderWave {
+  constructor(message) {
+      this._samples = 10000;
+      this._strokeStyle = "#3098ff";
+      this.audioContext = new AudioContext();
+      this.canvas = document.querySelector("#canvas_waveform");
+      this.ctx = this.canvas.getContext("2d");
+      this.isPlaying = 0; // Flag to track if audio is currently playing
+      this.playheadPosition = 0; // Current position of the playhead (slider)
+      this.data = [];
+      message
+          .then(arrayBuffer => {
+          return this.audioContext.decodeAudioData(arrayBuffer);
+      })
+          .then(audioBuffer => {
+          this.draw(this.normalizedData(audioBuffer));
+          this.drawData(this.data);
+      });
+  }
+  normalizedData(audioBuffer) {
+      const rawData = audioBuffer.getChannelData(0); // We only need to work with one channel of data
+      const samples = this._samples; // Number of samples we want to have in our final data set
+      const blockSize = Math.floor(rawData.length / samples); // Number of samples in each subdivision
+      const filteredData = [];
+      for (let i = 0; i < samples; i++) {
+          filteredData.push(rawData[i * blockSize]);
+      }
+      return filteredData;
+  }
+  draw(normalizedData) {
+      // set up the canvas
+      const canvas = this.canvas;
+      const dpr = window.devicePixelRatio || 1;
+      const padding = 10;
+      canvas.width = canvas.offsetWidth * dpr;
+      canvas.height = (canvas.offsetHeight + padding * 2) * dpr;
+      this.ctx.scale(dpr, dpr);
+      this.ctx.translate(0, canvas.offsetHeight / 2 + padding); // set Y = 0 to be in the middle of the canvas
+      // draw the line segments
+      const width = canvas.offsetWidth / normalizedData.length;
+      for (let i = 0; i < normalizedData.length; i++) {
+          const x = width * i;
+          let height = normalizedData[i] * canvas.offsetHeight - padding;
+          if (height < 0) {
+              height = 0;
+          }
+          else if (height > canvas.offsetHeight / 2) {
+              height = height > canvas.offsetHeight / 2;
+          }
+          // this.drawLineSegment(this.ctx, x, height, width, (i + 1) % 2);
+          this.data.push({
+              x: x,
+              h: height,
+              w: width,
+              isEven: (i + 1) % 2
+          });
+      }
+      return this.data;
+  }
+  drawLineSegment(ctx, x, height, width, isEven, colors = this._strokeStyle) {
+      ctx.lineWidth = 1; // how thick the line is
+      ctx.strokeStyle = colors; // what color our line is
+      ctx.beginPath();
+      height = isEven ? height : -height;
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x + width, height);
+      ctx.stroke();
+  }
+
+  drawData(data) {
+      data.forEach(item => {
+        const colors = this.isPlaying && item.x <= this.playheadPosition * this.canvas.offsetWidth
+          ? "#000000" // Change color to black if it's part of the audio being played
+          : this._strokeStyle; // Otherwise, use the original color
+          this.drawLineSegment(this.ctx, item.x, item.h, item.w, item.isEven, colors);
+      });
+  }
+
+  drawTimeline(percent) {
+      let end = Math.ceil(this._samples * percent);
+      let start = end - 5 || 0;
+      let t = this.data.slice(0, end);
+      this.drawData(t, "#1d1e22");
+      this.playheadPosition = percent;
+  }
+}
+
 document
   .getElementById("startRecording")
   .addEventListener("click", initFunction);
@@ -27,20 +116,44 @@ function initFunction() {
   }
 
   isRecording.textContent = "Recording...";
-  //
-
+  
   let audioChunks = [];
   let rec;
 
   function handlerFunction(stream) {
     rec = new MediaRecorder(stream);
+    var audioPlayer = document.getElementById("audioElement");
     rec.start();
     rec.ondataavailable = (e) => {
       audioChunks.push(e.data);
       if (rec.state == "inactive") {
-        let blob = new Blob(audioChunks, { type: "audio/mp3" });
+        let blob = new Blob(audioChunks, { type: "audio/wav" });
         console.log(blob);
-        document.getElementById("audioElement").src = URL.createObjectURL(blob);
+        audioPlayer.src = URL.createObjectURL(blob);
+
+        // Tear down after recording.
+        rec.stream.getTracks().forEach(t => t.stop())
+        // rec = null
+
+        if (audioChunks.length > 0) {
+          // Initialize renderWave after recording is stopped
+          const wave = new renderWave(audioChunks[0].arrayBuffer());
+
+          audioPlayer.addEventListener("play", () => {
+            wave.isPlaying = 1;});
+          audioPlayer.addEventListener("pause", () => {
+              wave.isPlaying = 0;});
+    
+          audioPlayer.addEventListener('ended', function() {
+            wave.isPlaying = 0;});
+    
+          audioPlayer.addEventListener("timeupdate", function () {
+            let percent = this.currentTime / this.duration;
+            wave.drawTimeline(percent);
+          });
+        }else {
+          console.log("No recorded audio available.");
+        }
       }
     };
   }
@@ -53,9 +166,32 @@ function initFunction() {
 
   startusingBrowserMicrophone(true);
 
+  function saveRecording() {
+    console.log(audioChunks)
+    if (audioChunks.length === 0) {
+      console.log("No recorded audio available.");
+      return;
+    }
+  
+    const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(audioBlob);
+    link.download = "recorded_audio.wav";
+    link.click();
+
+     // Clear the audioChunks array
+     audioChunks = [];
+  }
+
   // Stoping handler
   document.getElementById("stopRecording").addEventListener("click", (e) => {
     rec.stop();
     isRecording.textContent = "Click 'Play' button to start listening";
+
+    console.log(audioChunks)
+
   });
+
+  
+  document.getElementById("saveRecording").addEventListener("click", saveRecording);
 }
